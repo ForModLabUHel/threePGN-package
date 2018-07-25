37ed62eb16ba135c3d7600536e742be162e2f734
checcomi <checcomi@gmail.com>
2018-07-25 16:38:27 +0100

    small changes

--- r3pg/inst/examples/r3pgnHelp.R
+++ r3pg/inst/examples/r3pgnHelp.R
@@ -1,5 +1,4 @@
-firstRun <- r3pgn(siteData = data_site, climate = data_climate, parameters = data_param[,2],
-                  outputs = 1:10)
+firstRun <- r3pgn(siteData = data_site, climate = data_climate, parameters = data_param[,2])
 
 print(firstRun)
 plot(firstRun)
